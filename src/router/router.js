b801d7dc99621471562a13bd80c9640bc444f590
DeveloperLiao <1983790208@qq.com>
2022-09-02 08:47:36 +0800

    update in 9/2

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,4 @@
 // 路由模块
-import Home from '@/pages/Home/index.vue'
 import Login from '@/pages/Login/index.vue'
 import Register from '@/pages/Register/index.vue'
 import Search from '@/pages/Search/index.vue'
@@ -16,23 +15,24 @@ export default [
   { path: '/', redirect: '/home' },
   {
     path: '/home',
-    component: Home,
+    // 路由懒加载
+    component: () => import('@/pages/Home/index.vue'),
     //   路由元控制谁可以显示footer组件
     meta: { isShow: true }
   },
   {
     path: '/login',
-    component: Login,
+    component: () => import('@/pages/Login/index.vue'),
     meta: { isShow: false }
   },
   {
     path: '/register',
-    component: Register,
+    component: () => import('@/pages/Register/index.vue'),
     meta: { isShow: false }
   },
   {
     path: '/search',
-    component: Search,
+    component: () => import('@/pages/Search/index.vue'),
     meta: { isShow: true },
     // 给路由命名，具有唯一性
     name: 'search'
@@ -40,30 +40,30 @@ export default [
   {
     // 路由动态传参
     path: '/detail/:skuid',
-    component: Detail,
+    component: () => import('@/pages/Detail/index.vue'),
     meta: { isShow: true },
     name: 'detail'
   },
   {
     path: '/addcartsuccess',
     name: 'addcartsuccess',
-    component: Addcartsuccess,
+    component: () => import('@/pages/Addcartsuccess/index.vue'),
     meta: { isShow: true }
   },
   {
     path: '/shopcart',
     name: 'shopcart',
-    component: ShopCart,
+    component: () => import('@/pages/ShopCart/index.vue'),
     meta: { isShow: true }
   },
   {
     path: '/trade',
-    component: Trade,
+    component: () => import('@/pages/Trade/index.vue'),
     meta: { isShow: true }
   },
   {
     path: '/pay/:orderId',
-    component: Pay,
+    component: () => import('@/pages/Pay/index.vue'),
     meta: { isShow: false },
     //路由独享守卫,只能接受来自trade的路由
     beforeEnter: (to, from) => {
@@ -77,7 +77,7 @@ export default [
   },
   {
     path: '/paysuccess',
-    component: PaySuccess,
+    component: () => import('@/pages/PaySuccess/index.vue'),
     meta: { isShow: false },
     //路由独享守卫,只能接受来自pay的路由
     beforeEnter: (to, from) => {
@@ -92,11 +92,11 @@ export default [
   {
     path: '/center',
     redirect: { path: '/center/myorder' },
-    component: Center,
+    component: () => import('@/pages/Center/index.vue'),
     meta: { isShow: false },
     children: [
-      { path: 'myorder', component: MyOrder },
-      { path: 'grouporder', component: GroupOrder }
+      { path: 'myorder', component: () => import('@/pages/MyOrder/index.vue') },
+      { path: 'grouporder', component: () => import('@/pages/GroupOrder/index.vue') }
     ]
   }
 ]
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ const webpack = require('webpack')
 const { VantResolver } = require('unplugin-vue-components/resolvers')
 const ComponentsPlugin = require('unplugin-vue-components/webpack')
 module.exports = defineConfig({
+  // 项目进行打包后,代码是进过压缩加密的，如果运行时报错，输出的错误信息无法准确得知是哪里的代码报错
+  // 有了map就可以像未加密的代码一样，准确的输出是那一行那一列有错
+  // 所以该文件如果项目不需要是可以去除掉
+  productionSourceMap: false,
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
